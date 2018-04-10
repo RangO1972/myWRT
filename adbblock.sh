7c8f8e549672540b030d8d610cf0e6ac630e9265
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2018-04-10 22:29:28 +0200

    Update adbblock.sh

--- adbblock.sh
+++ adbblock.sh
@@ -1,1 +1,22 @@
-test
+#!/bin/ash
+
+HOST_LIST=/tmp/myhostlist
+TMP_HOSTS=/tmp/block.hosts.unsorted
+HOSTS=/tmp/block.hosts
+
+# remove any old TMP_HOSTS that might have stuck around
+rm ${TMP_HOSTS} 2> /dev/null
+rm ${HOST_LIST} 2> /dev/null
+
+# Download the host file list
+grep -v '^#' ~/${HOST_LIST} | wget -i- -qO- | grep -v -e "^#" -e "^\s*$" -e "\blocalhost\b" | sed -E -e "s/^127.0.0.1/0.0.0.0/" -e "s/#.*$//" -e "s/\t/ /" -e "s/[[:space:]]{2,}/ /" | tr -d "\r" >> ${TMP_HOSTS}
+
+# remove duplicate hosts and save the real hosts file
+sort ${TMP_HOSTS} | uniq > ${HOSTS}
+
+
+
+#rm ${TMP_HOSTS} 2> /dev/null
+#rm ${HOST_LIST} 2> /dev/null
+
+killall -s SIGHUP dnsmasq
