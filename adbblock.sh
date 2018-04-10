d0263700d6e3ea28a176a9644226c6ff5f4a4772
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2018-04-10 22:52:36 +0200

    Update adbblock.sh

--- adbblock.sh
+++ adbblock.sh
@@ -8,8 +8,26 @@ HOSTS=/tmp/block.hosts
 rm ${TMP_HOSTS} 2> /dev/null
 rm ${HOST_LIST} 2> /dev/null
 
-# Download the host file list
-wget -i ${HOST_LIST} -qO- | grep -v '^#'  | wget -i- -qO- | grep -v -e "^#" -e "^\s*$" -e "\blocalhost\b" | sed -E -e "s/^127.0.0.1/0.0.0.0/" -e "s/#.*$//" -e "s/\t/ /" -e "s/[[:space:]]{2,}/ /" | tr -d "\r" >> ${TMP_HOSTS}
+
+
+# grab a hosts file list and...
+# filter out comment lines
+# filter out empty lines
+# replace double+ spaces with single spaces
+# remove carriage returns
+wget -i  -qO- | grep -v -e "^#" -e "^\s*$" | sed -E -e "s/[[:space:]]{2,}/ /" | tr -d "\r" >> ${HOST_LIST}
+
+# grab a hosts file list and...
+# filter out comment lines
+# filter out empty lines
+# filter out localhost entries (the router is handling localhost)
+# replace 127.0.0.1 with 0.0.0.0
+# remove trailing comments
+# replace tabs with spaces
+# replace double+ spaces with single spaces
+# remove carriage returns
+
+wget -i ${HOST_LIST} -qO- | grep -v -e "^#" -e "^\s*$" -e "\blocalhost\b" | sed -E -e "s/^127.0.0.1/0.0.0.0/" -e "s/#.*$//" -e "s/\t/ /" -e "s/[[:space:]]{2,}/ /" | tr -d "\r" >> ${TMP_HOSTS}
 
 # remove duplicate hosts and save the real hosts file
 sort ${TMP_HOSTS} | uniq > ${HOSTS}
@@ -19,4 +37,4 @@ sort ${TMP_HOSTS} | uniq > ${HOSTS}
 #rm ${TMP_HOSTS} 2> /dev/null
 #rm ${HOST_LIST} 2> /dev/null
 
-killall -s SIGHUP dnsmasq
+#killall -s SIGHUP dnsmasq
