120b70064e325b30712590fa2d9c422a0d5f43d0
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2018-04-10 22:35:07 +0200

    Update adbblock.sh

--- adbblock.sh
+++ adbblock.sh
@@ -9,7 +9,7 @@ rm ${TMP_HOSTS} 2> /dev/null
 rm ${HOST_LIST} 2> /dev/null
 
 # Download the host file list
-grep -v '^#' ~/${HOST_LIST} | wget -i- -qO- | grep -v -e "^#" -e "^\s*$" -e "\blocalhost\b" | sed -E -e "s/^127.0.0.1/0.0.0.0/" -e "s/#.*$//" -e "s/\t/ /" -e "s/[[:space:]]{2,}/ /" | tr -d "\r" >> ${TMP_HOSTS}
+grep -v '^#' ${HOST_LIST} | wget -i- -qO- | grep -v -e "^#" -e "^\s*$" -e "\blocalhost\b" | sed -E -e "s/^127.0.0.1/0.0.0.0/" -e "s/#.*$//" -e "s/\t/ /" -e "s/[[:space:]]{2,}/ /" | tr -d "\r" >> ${TMP_HOSTS}
 
 # remove duplicate hosts and save the real hosts file
 sort ${TMP_HOSTS} | uniq > ${HOSTS}
